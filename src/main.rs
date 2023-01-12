15558ca73a7e9acffc87fbee4886ce63349cac2a
zhang.ning29 <zhang.ning29@byd.com>
2023-01-12 13:28:28 +0800

    Pin在实践中的运用,将值固定在栈上

--- src/main.rs
+++ src/main.rs
@@ -1,110 +1,51 @@
-use {
-    futures::{
-        future::{BoxFuture, FutureExt},
-        task::{waker_ref, ArcWake},
-    },
-    std::{
-        future::Future,
-        sync::mpsc::{sync_channel, Receiver, SyncSender},
-        sync::{Arc, Mutex},
-        task::{Context, Poll},
-        time::Duration,
-    },
-    // 引入之前实现的定时器模块
-    timer_future::TimerFuture,
-};
 
-/// 任务执行器，负责从通道中接收任务然后执行
-struct Executor {
-    ready_queue: Receiver<Arc<Task>>,
+use std::pin::Pin;
+use std::marker::PhantomPinned;
+
+#[derive(Debug)]
+struct Test {
+    a: String,
+    b: *const String,
+    _marker: PhantomPinned,
 }
 
-/// `Spawner`负责创建新的`Future`然后将它发送到任务通道中
-#[derive(Clone)]
-struct Spawner {
-    task_sender: SyncSender<Arc<Task>>,
-}
 
-/// 一个Future，它可以调度自己(将自己放入任务通道中)，然后等待执行器去`poll`
-struct Task {
-    /// 进行中的Future，在未来的某个时间点会被完成
-    ///
-    /// 按理来说`Mutex`在这里是多余的，因为我们只有一个线程来执行任务。但是由于
-    /// Rust并不聪明，它无法知道`Future`只会在一个线程内被修改，并不会被跨线程修改。因此
-    /// 我们需要使用`Mutex`来满足这个笨笨的编译器对线程安全的执着。
-    ///
-    /// 如果是生产级的执行器实现，不会使用`Mutex`，因为会带来性能上的开销，取而代之的是使用`UnsafeCell`
-    future: Mutex<Option<BoxFuture<'static, ()>>>,
-
-    /// 可以将该任务自身放回到任务通道中，等待执行器的poll
-    task_sender: SyncSender<Arc<Task>>,
-}
-
-fn new_executor_and_spawner() -> (Executor, Spawner) {
-    // 任务通道允许的最大缓冲数(任务队列的最大长度)
-    // 当前的实现仅仅是为了简单，在实际的执行中，并不会这么使用
-    const MAX_QUEUED_TASKS: usize = 10_000;
-    let (task_sender, ready_queue) = sync_channel(MAX_QUEUED_TASKS);
-    (Executor { ready_queue }, Spawner { task_sender })
-}
-
-impl Spawner {
-    fn spawn(&self, future: impl Future<Output = ()> + 'static + Send) {
-        let future = future.boxed();
-        let task = Arc::new(Task {
-            future: Mutex::new(Some(future)),
-            task_sender: self.task_sender.clone(),
-        });
-        self.task_sender.send(task).expect("任务队列已满");
-    }
-}
-
-impl ArcWake for Task {
-    fn wake_by_ref(arc_self: &Arc<Self>) {
-        // 通过发送任务到任务管道的方式来实现`wake`，这样`wake`后，任务就能被执行器`poll`
-        let cloned = arc_self.clone();
-        arc_self
-            .task_sender
-            .send(cloned)
-            .expect("任务队列已满");
-    }
-}
-
-impl Executor {
-    fn run(&self) {
-        while let Ok(task) = self.ready_queue.recv() {
-            // 获取一个future，若它还没有完成(仍然是Some，不是None)，则对它进行一次poll并尝试完成它
-            let mut future_slot = task.future.lock().unwrap();
-            if let Some(mut future) = future_slot.take() {
-                // 基于任务自身创建一个 `LocalWaker`
-                let waker = waker_ref(&task);
-                let context = &mut Context::from_waker(&*waker);
-                // `BoxFuture<T>`是`Pin<Box<dyn Future<Output = T> + Send + 'static>>`的类型别名
-                // 通过调用`as_mut`方法，可以将上面的类型转换成`Pin<&mut dyn Future + Send + 'static>`
-                if future.as_mut().poll(context).is_pending() {
-                    // Future还没执行完，因此将它放回任务中，等待下次被poll
-                    *future_slot = Some(future);
-                }
-            }
+impl Test {
+    fn new(txt: &str) -> Self {
+        Test {
+            a: String::from(txt),
+            b: std::ptr::null(),
+            _marker: PhantomPinned, // 这个标记可以让我们的类型自动实现特征`!Unpin`
         }
     }
+
+    fn init(self: Pin<&mut Self>) {
+        let self_ptr: *const String = &self.a;
+        let this = unsafe { self.get_unchecked_mut() };
+        this.b = self_ptr;
+    }
+
+    fn a(self: Pin<&Self>) -> &str {
+        &self.get_ref().a
+    }
+
+    fn b(self: Pin<&Self>) -> &String {
+        assert!(!self.b.is_null(), "Test::b called without Test::init being called first");
+        unsafe { &*(self.b) }
+    }
 }
+pub fn main() {
+   // 此时的`test1`可以被安全的移动
+   let mut test1 = Test::new("test1");
+   // 新的`test1`由于使用了`Pin`，因此无法再被移动，这里的声明会将之前的`test1`遮蔽掉(shadow)
+   let mut test1 = unsafe { Pin::new_unchecked(&mut test1) };
+   Test::init(test1.as_mut());
 
-fn main() {
-    let (executor, spawner) = new_executor_and_spawner();
+   let mut test2 = Test::new("test2");
+   let mut test2 = unsafe { Pin::new_unchecked(&mut test2) };
+   Test::init(test2.as_mut());
 
-    // 生成一个任务
-    spawner.spawn(async {
-        println!("howdy!");
-        // 创建定时器Future，并等待它完成
-        TimerFuture::new(Duration::new(2, 0)).await;
-        println!("done!");
-    });
-
-    // drop掉任务，这样执行器就知道任务已经完成，不会再有新的任务进来
-    drop(spawner);
-
-    // 运行执行器直到任务队列为空
-    // 任务运行后，会先打印`howdy!`, 暂停2秒，接着打印 `done!`
-    executor.run();
+   println!("a: {}, b: {}", Test::a(test1.as_ref()), Test::b(test1.as_ref()));
+   std::mem::swap(test1.get_mut(), test2.get_mut());
+   println!("a: {}, b: {}", Test::a(test2.as_ref()), Test::b(test2.as_ref()));
 }
