3e1f29404e1368659e4262b14d1bd88bb22c4403
zhang.ning29 <zhang.ning29@byd.com>
2023-07-07 11:01:40 +0800

    不太优秀的单向链表

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 
+#![allow(unused)]
 use std::pin::Pin;
 use std::marker::PhantomPinned;
-use std::mem;
 
 #[derive(Debug)]
 struct Test {
@@ -35,28 +35,19 @@ impl Test {
         unsafe { &*(self.b) }
     }
 }
+
 pub fn main() {
-   /* // 此时的`test1`可以被安全的移动
-   let mut test1 = Test::new("test1");
-   // 新的`test1`由于使用了`Pin`，因此无法再被移动，这里的声明会将之前的`test1`遮蔽掉(shadow)
-   let mut test1 = unsafe { Pin::new_unchecked(&mut test1) };
-   Test::init(test1.as_mut());
+    // 此时的`test1`可以被安全的移动
+    let mut test1 = Test::new("test1");
+    // 新的`test1`由于使用了`Pin`，因此无法再被移动，这里的声明会将之前的`test1`遮蔽掉(shadow)
+    let mut test1 = unsafe { Pin::new_unchecked(&mut test1) };
+    Test::init(test1.as_mut());
 
-   let mut test2 = Test::new("test2");
-   let mut test2 = unsafe { Pin::new_unchecked(&mut test2) };
-   Test::init(test2.as_mut());
+    let mut test2 = Test::new("test2");
+    let mut test2 = unsafe { Pin::new_unchecked(&mut test2) };
+    Test::init(test2.as_mut());
 
-   println!("a: {}, b: {}", Test::a(test1.as_ref()), Test::b(test1.as_ref()));
-   std::mem::swap(test1.get_mut(), test2.get_mut());
-   println!("a: {}, b: {}", Test::a(test2.as_ref()), Test::b(test2.as_ref())); */
-   let mut test1 = Test::new("test1");
-   let mut test1_pin = unsafe { Pin::new_unchecked(&mut test1) };
-   Test::init(test1_pin.as_mut());
-
-   drop(test1_pin);
-   println!(r#"test1.b points to "test1": {:?}..."#, test1.b);
-
-   let mut test2 = Test::new("test2");
-   mem::swap(&mut test1, &mut test2);
-   println!("... and now it points nowhere: {:?}", test1.b);
+    println!("a: {}, b: {}", Test::a(test1.as_ref()), Test::b(test1.as_ref()));
+    std::mem::swap(test1.get_mut(), test2.get_mut());
+    println!("a: {}, b: {}", Test::a(test2.as_ref()), Test::b(test2.as_ref()));
 }
